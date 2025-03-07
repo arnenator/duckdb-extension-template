f69927526105f87c24ff76bdf21899a20983db07
Andreas Tietgen <9461145+arnenator@users.noreply.github.com>
2025-03-07 08:36:40 +0100

    Update description in the custom toolchain script

--- scripts/setup-custom-toolchain.sh
+++ scripts/setup-custom-toolchain.sh
@@ -3,8 +3,8 @@
 # This is an example script that can be used to install additional toolchain dependencies. Feel free to remove this script
 # if no additional toolchains are required
 
-# To enable this script, set the `custom_toolchain_script` option to true when calling the reusable workflow
-# `.github/workflows/_extension_distribution.yml` from `https://github.com/duckdb/extension-ci-tools`
+# To use this script in CI github actions, you would need to override the ´configure_ci´ target in your makefile and have it call this script 
+# e.g. 'bash ./scripts/setup-custom-toolchain.sh'
 
 # note that the $DUCKDB_PLATFORM environment variable can be used to discern between the platforms
 echo "This is the sample custom toolchain script running for architecture '$DUCKDB_PLATFORM' for the quack extension."
